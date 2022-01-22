2f1dd2fb63eea99b5fb0fda51b89501cf4789c0e
faimtonmoy <bhuiyanfaim@gmail.com>
2022-01-22 22:11:50 +0600

    initial commit

---
+++ Components/Layout.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import React from "react";
+import Link from "next/link";
+import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
+import useStyles from "../utils/styles";
+export default function LayOut({ children }) {
+  const classes = useStyles();
+  return (
+    <div>
+      <Head>
+        <title>Online-Shop</title>
+      </Head>
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
+          <Link href="/">
+            <a className={classes.brand}>Online-Shop</a>
+          </Link>
+          <div className={classes.grow}> </div>
+              <div>
+              <Link href="/myorder">
+                  <a>My Order</a>
+              </Link>
+              <Link href="/cart">
+                  <a>Cart</a>
+              </Link>
+              </div>
+         
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.main}>{children}</Container>
+    </div>
+  );
+}
