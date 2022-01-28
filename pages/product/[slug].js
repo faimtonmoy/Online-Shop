0fa6c4de003886c874d7bee11dbe2882a6ed7344
faimtonmoy <bhuiyanfaim@gmail.com>
2022-01-28 21:09:46 +0600

    get static path and props added

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -6,11 +6,12 @@ import Link from "next/link";
 import useStyles from "../../utils/styles";
 import { Button, Card, Grid } from "@material-ui/core";
 import Image from "next/image";
-export default function ProductDetails() {
+import db from "../../utils/db";
+import Product from "../../models/Product";
+export default function ProductDetails(props) {
+  const {product}= props;
   const classes = useStyles();
-  const router = useRouter();
-  const { slug } = router.query;
-  const product = data.products.find((a) => a.slug === slug);
+ 
   if (!product) {
     return <div>Product Not Available</div>;
   }
@@ -52,4 +53,30 @@ export default function ProductDetails() {
       </Grid>
     </Layout>
   );
+}
+export async function getStaticPaths() {
+  await db.connect();
+  const products= await Product.find({}).lean();
+  await db.disconnect();
+  const paths= products.map((product)=>({
+    params: {slug: product.slug}
+  }));
+  return {
+    paths,
+    fallback: true 
+  };
+}
+export async function getStaticProps(context)
+{
+  const {params}= context;
+  const {slug}= params;
+  await db.connect();
+  const product= await Product.findOne({slug}).lean();
+  await db.disconnect();
+  return{
+    props:{
+      product: db.convertDocToObj(product),
+    }
+    
+  };
 }
